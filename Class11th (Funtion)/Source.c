831f2b5b99aafebe92827cf85d43e27c74b3790f
mhkhil <mhkhilrr@gmail.com>
2024-08-30 21:46:14 +0900

    (2024.08.30) 랜덤 함수

--- Class11th (Funtion)/Source.c
+++ Class11th (Funtion)/Source.c
@@ -62,16 +62,23 @@ int main()
 #pragma endregion
 
 #pragma region 자기 참조 구조체
-	struct Node node1 = { 10, NULL };
-	struct Node node2 = { 20, NULL };
-	struct Node node3 = { 30, NULL };
-
-	
-	node1.next = &node2;
-	node2.next = &node3;
-	node3.next = NULL;
-
-	
+	// struct Node node1 = { 10, NULL };
+	// struct Node node2 = { 20, NULL };
+	// struct Node node3 = { 30, NULL };
+	// 
+	// 
+	// node1.next = &node2;
+	// node2.next = &node3;
+	// node3.next = NULL;
+	// 
+	// struct Node* currentNode = &node1;
+	// 
+	// while (currentNode != NULL)
+	// {
+	// 	printf("data의 값 : %d\n", currentNode->data);
+	// 
+	// 	currentNode = currentNode->next;
+	// }
 
 #pragma endregion
 
